0b4936167b7853992ca4801d3f6ccdafe05494a8
Rohnak Agarwal <rrka79wal@gmail.com>
2024-06-13 15:22:02 +0530

    TF started

---
+++ by-books/Deep Learning with Applications Using Python - Chatbots and Face, Object, and Speech Recognition with Tensorflow and Keras/readme.md
@@ -0,0 +1,5 @@
+# Deep Learning with Applications Using Python: Chatbots and Face, Object, and Speech Recognition with Tensorflow and Keras
+
+https://drive.google.com/file/d/1A8HSxeJQtIIDYUmUFCsmwhQI_857D0Kz/view
+
+**Progress**: 22 / 227
--- install_req.sh
+++ install_req.sh
@@ -3,8 +3,14 @@
 # the script will terminate as soon as any command inside it fails
 set -e
 
-pip install ./subjects/NLP/en_core_web_sm-3.7.1-py3-none-any.whl
-pip install -U -r ./requirements.txt
-pip-chill --no-version > ./requirements.txt
+contents=$(cat ./requirements.txt)
+pchill=$(pip-chill --no-version)
 
-echo "DONE!"
+if [[ "$contents" != "$pchill" ]]; then
+    pip install ./subjects/NLP/en_core_web_sm-3.7.1-py3-none-any.whl
+    pip install -U -r ./requirements.txt
+    pip-chill --no-version > ./requirements.txt
+    echo "DONE!"
+else
+    echo "All up-to-date!"
+fi
